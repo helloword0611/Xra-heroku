131c1433542fb9fef16770190a487d928705980f
helloword0611 <94452062+helloword0611@users.noreply.github.com>
2022-01-24 08:59:53 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="08053b44-f5e7-48e0-bd93-ed2432f25a0e"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="08053b44-f5e7-48e0-bd93-ed2432f25a0e"
 fi
 echo ${Vmess_UUID}
 
